fda92fd663622cc4e73c90b3d92f914760f788a4
AryanKDC <aryan.kdigitalcurry@gmail.com>
2025-02-26 18:31:23 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,85 +1,175 @@
-import './Footer.css';
-import logo from '../images/KDC_logo.png';
-import facebook from '../images/Facebook.png';
-import instagram from '../images/Instagram.png';
-import linkedin from '../images/Linkedin.png';
-import { useInView } from 'react-intersection-observer';
-import { motion } from 'framer-motion';
+import { Box, Typography, Stack, Link } from "@mui/material";
+import kdclogo from "../images/KDC_logo.png";
+import facebookimg from "../images/Facebook.png";
+import instagramimg from "../images/Instagram.png";
+import linkedinimg from "../images/Linkedin.png";
 
-function Footer() {
-    const fadeInVariants = {
-        hidden: { opacity: 0, y: 50 },
-        visible: { opacity: 1, y: 0, transition: { duration: 0.6 } }
-    };
-
-    const [refOne, inViewOne] = useInView({ triggerOnce: true, threshold: 0.2 });
-    const [refTwo, inViewTwo] = useInView({ triggerOnce: true, threshold: 0.2 });
-    const [refThree, inViewThree] = useInView({ triggerOnce: true, threshold: 0.2 });
-
+const Footer = () => {
     return (
-        <div className='footer'>
-            <footer>
-                <motion.div
-                    ref={refOne}
-                    initial="hidden"
-                    animate={inViewOne ? "visible" : "hidden"}
-                    variants={fadeInVariants}
-                    className='col-one'
+        <footer>
+            <Box
+                sx={{
+                    backgroundColor: "#fff",
+                    py: 4,
+                    px: { xs: "20px", sm: "40px", md: "100px" },
+                    mb: 0,
+                    borderTop: "1px solid black",
+                    textAlign: { xs: "center", md: "left" },
+                }}
+            >
+                <Stack
+                    direction={{ xs: "column", md: "row" }}
+                    justifyContent="space-between"
+                    alignItems={{ xs: "center", md: "flex-start" }}
+                    flexWrap="wrap"
+                    gap={{ xs: 4, md: 0 }}
                 >
-                    <img src={logo} alt='logo'/>
-                    <p>KDgitialCurry is a Leading Mobile and Web App 
-                    Development Company in Mumbai with over 10+ Years of Experience. 
-                    We create human-centric, scalable solutions delivering full-stack services 
-                    across various domains.</p>
+                    {/* Left Column - Logo & Description */}
+                    <Box
+                        sx={{
+                            maxWidth: { xs: "100%", md: "350px" }, // Keep width at 350px for md screens
+                            "@media (min-width: 768px)": { maxWidth: "550px" }, // Ensure max-width at 1240px
+                            "@media (min-width: 1240px)": { maxWidth: "350px" },
+                        }}
+                    >
+                        <img
+                            src={kdclogo}
+                            alt="KDigitalCurry"
+                            width="170px"
+                            height="auto"
+                        />
+                        <Typography variant="body2" color="text.secondary" mt={1}>
+                            KDigitalCurry is a leading mobile and web app development company
+                            in Mumbai with over 10+ years of experience. We create
+                            human-centric, scalable solutions, delivering full-stack services
+                            across various domains.
+                        </Typography>
 
-                    <div className='socio-icon'>
-                        <img src={facebook} alt='facebook'/>
-                        <img src={instagram} alt='instagram' />
-                        <img src={linkedin} alt='linkedin'/>
-                    </div>
-                </motion.div>
+                        {/* Social Icons with Links */}
+                        <Stack
+                            direction="row"
+                            spacing={2}
+                            mt={2}
+                            justifyContent={{ xs: "center", md: "flex-start" }}
+                        >
+                            <Link href="https://www.facebook.com" target="_blank">
+                                <img
+                                    src={facebookimg}
+                                    alt="Facebook"
+                                    style={{ height: "35px", width: "35px" }}
+                                />
+                            </Link>
+                            <Link href="https://www.instagram.com" target="_blank">
+                                <img
+                                    src={instagramimg}
+                                    alt="Instagram"
+                                    style={{ height: "35px", width: "35px" }}
+                                />
+                            </Link>
+                            <Link href="https://www.linkedin.com" target="_blank">
+                                <img
+                                    src={linkedinimg}
+                                    alt="LinkedIn"
+                                    style={{ height: "35px", width: "35px" }}
+                                />
+                            </Link>
+                        </Stack>
+                    </Box>
 
-                <motion.div
-                    ref={refTwo}
-                    initial="hidden"
-                    animate={inViewTwo ? "visible" : "hidden"}
-                    variants={fadeInVariants}
-                    className='col-two'
+                    {/* Middle Column - Services */}
+                    <Box
+                        sx={{
+                            ml: { md: "20px" },
+                            textAlign: { xs: "center", md: "left" },
+                        }}
+                    >
+                        <Typography fontWeight="bold" sx={{ mb: 1 }}>
+                            Services
+                        </Typography>
+                        <Box>
+                            {[
+                                "Mobile App Development",
+                                "Web & CMS Development",
+                                "ECommerce Development",
+                                "Search Engine Optimization",
+                                "Hire Dedicated Resources",
+                                "UI / UX Design",
+                                "Custom Software Development",
+                            ].map((service, index) => (
+                                <Link
+                                    key={index}
+                                    href="#"
+                                    sx={{
+                                        display: "block",
+                                        my: 0.8,
+                                        textDecoration: "none",
+                                        color: "black",
+                                        fontSize: "15px",
+                                        fontWeight: "normal",
+                                        mb: { md: "10px" },
+                                        "&:hover": { textDecoration: "underline" },
+                                    }}
+                                >
+                                    • {service}
+                                </Link>
+                            ))}
+                        </Box>
+                    </Box>
+
+                    {/* Right Column - Careers */}
+                    <Box
+                        sx={{
+                            ml: { md: "60px" },
+                            mr: { md: "160px" },
+                            textAlign: { xs: "center", md: "left" },
+                        }}
+                    >
+                        <Typography fontWeight="bold" sx={{ mb: 1 }}>
+                            Explore Careers
+                        </Typography>
+                        <Box>
+                            {[
+                                "Sr. PHP Developer",
+                                "Sr. MERN Developer",
+                                "IOS Developer",
+                                "UI / UX Designer",
+                                "Flutter Developer",
+                                "Project Manager",
+                                "Android Developer",
+                            ].map((career, index) => (
+                                <Link
+                                    key={index}
+                                    href="#"
+                                    sx={{
+                                        display: "block",
+                                        my: 0.8,
+                                        textDecoration: "none",
+                                        color: "black",
+                                        fontSize: "15px",
+                                        fontWeight: "normal",
+                                        mb: { md: "10px" },
+                                        "&:hover": { textDecoration: "underline" },
+                                    }}
+                                >
+                                    • {career}
+                                </Link>
+                            ))}
+                        </Box>
+                    </Box>
+                </Stack>
+
+                {/* Bottom Copyright */}
+                <Typography
+                    variant="body2"
+                    textAlign="center"
+                    mt={7}
+                    color="text.secondary"
                 >
-                    <p>Services</p>
-                    <ul>
-                        <li>Mobile App Development</li>
-                        <li>Web & CMS Development</li>
-                        <li>ECommerce Development</li>
-                        <li>Search Engine Optimization</li>
-                        <li>Hire Dedicated Resource</li>
-                        <li>UI / UX Design</li>
-                        <li>Custom Software Development</li>
-                    </ul>
-                </motion.div>
-
-                <motion.div
-                    ref={refThree}
-                    initial="hidden"
-                    animate={inViewThree ? "visible" : "hidden"}
-                    variants={fadeInVariants}
-                    className='col-three'
-                >
-                    <p>Explore Careers</p>
-                    <ul>
-                        <li>Sr. PHP Developer</li>
-                        <li>Sr. MERN Developer</li>
-                        <li>IOS Developer</li>
-                        <li>UI / UX Designer</li>
-                        <li>Flutter Developer</li>
-                        <li>Project Manager</li>
-                        <li>Android Developer</li>
-                    </ul>
-                </motion.div>
-            </footer>
-            <p className='copyright'>Copyright @ 2024 KDIGITALCURRY. All Rights Reserved</p>
-        </div>
+                    Copyright © 2024 K DIGITALCURRY. All Rights Reserved
+                </Typography>
+            </Box>
+        </footer>
     );
-}
+};
 
 export default Footer;
